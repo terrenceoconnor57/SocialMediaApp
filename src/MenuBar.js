141406a7ccba373996c1c32d311a85b1085ae66b
terrenceoconnor57 <60070754+terrenceoconnor57@users.noreply.github.com>
2020-07-11 19:30:26 -0400

    Created layout for application

---
+++ src/MenuBar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './MenuBar.css';
+
+class MenuBar extends Component {
+    render() {
+
+
+
+
+
+    return (
+
+      <div className = 'menugrid'>
+
+      <p className = 'logo'>Logo</p>
+      <p className = 'top'>About</p>
+      <p className = 'top'>API</p>
+
+      </div>
+
+
+    )
+}
+}
+
+export default MenuBar;
